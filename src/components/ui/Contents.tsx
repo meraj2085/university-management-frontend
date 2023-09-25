d9233aa1a64c7aea441fd15171bd814ebf3c056a
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-09-25 12:18:18 +0600

    Adding font

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,11 +1,32 @@
 "use client";
 
-import { Breadcrumb, Layout, Menu, theme } from "antd";
-const { Header, Content, Footer, Sider } = Layout;
+const { Content } = Layout;
+import { Layout } from "antd";
+import UMBreadCrumb from "./UMBreadCrumb";
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
+  const base = "admin";
   return (
-    <Content style={{ minHeight: "100vh", color: "black" }}>{children}</Content>
+    <Content
+      style={{
+        minHeight: "100vh",
+        color: "black",
+      }}
+    >
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
+      {children}
+    </Content>
   );
 };
 
